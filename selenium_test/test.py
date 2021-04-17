5864aac37a1d65c561f886f24ce9031ea4588356
AON <aon25650572@gmail.com>
2021-04-18 08:40:38 +0900

    changed

--- selenium_test/test.py
+++ selenium_test/test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Remote(
-    command_executor="http://192.168.0.2:4444/wd/hub",
+    command_executor="http://192.168.56.1:4444/wd/hub",
     options=chrome_options
     )
 driver.get("https://www.google.com/search?q=qiita")
